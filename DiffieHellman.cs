29748132c314cfa9bde442560b79a6e94d36a8f3
CyberMeerkat <apouyaud@gmail.com>
2022-09-27 13:12:15 +0200

    fixed indentation

--- DiffieHellman.cs
+++ DiffieHellman.cs
@@ -6,7 +6,7 @@ public static class DiffieHellman
     public static BigInteger PrivateKey(BigInteger primeP) => new Random().Next((int)primeP);
 
     public static BigInteger PublicKey(BigInteger primeP, BigInteger primeG, BigInteger privateKey) =>
-         BigInteger.ModPow(primeG, privateKey, primeP);
+        BigInteger.ModPow(primeG, privateKey, primeP);
 
     public static BigInteger Secret(BigInteger primeP, BigInteger publicKey, BigInteger privateKey) =>
         BigInteger.ModPow(publicKey, privateKey, primeP);
